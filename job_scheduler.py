aa08efcae23aa7bcaf80c04a1140fbbc69ba98de
SANDRAVS1234 <sandra.ds24@duk.ac.in>
2025-05-25 13:05:10 +0530

    Delete job_scheduler.py

--- job_scheduler.py
+++
@@ -1,6 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from scraper.job_scraper import scrape_jobs
-
-scheduler = BlockingScheduler()
-scheduler.add_job(scrape_jobs, 'interval', hours=24)
-scheduler.start()
